a51c08081cbcad61b1e099314f7193538d83e03a
Manish Timalsina <manishtimalsina51@gmail.com>
2024-06-19 12:06:00 +0545

    added class for pagination

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers;
 use App\Models\User;
 
+use App\Models\JobType;
+use App\Models\Category;
+use App\Models\Job;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
 class AccountController extends Controller
 {
     //user registration page
@@ -104,13 +108,128 @@ public function authenticate(Request $request){
     }
 
     public function profile(){
-       return view('frontend.account.profile');
+
+        $id = Auth::user()->id;
+       $user= User::find($id);
+
+       return view('frontend.account.profile',compact('user'));
+}
+
+public function updateUserProfile(Request $request){
+    $id = Auth::user()->id;
+    $validator = Validator::make($request->all(),[
+        'name' => 'required|min:5|max:20',
+        'email' => 'required|email|unique:users,email,'.$id.',id'
+    ]);
+
+    if($validator->passes()){
+        $user = User::find($id);
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->designation = $request->designation;
+        $user->phone = $request->phone;
+        $user->save();
+
+        session()->flash('success','Profile Updated Successfully. ');
+        return response()->json([
+            'status' => true,
+            'errors' =>[],
+        ]);
+    }else {
+        return response()->json([
+            'status' => false,
+            'errors' => $validator->errors(),
+        ]);
+    }
+}
+
+public function updateProfilePic(Request $request){
+    $id = Auth::user()->id;
+    $validator = Validator::make($request->all(), [
+        'image' => 'required|image'
+    ]);
+
+    if ($validator->passes()) {
+        $image = $request->file('image');
+        $ext = $image->getClientOriginalExtension();
+        $imageName = $id . '-' . time() . '.' . $ext;
+        $image->move(public_path('/profile_pic/'), $imageName);
+
+        User::where('id', $id)->update(['image' => $imageName]);
+
+        session()->flash('success', 'Profile Picture Updated Successfully');
+        return response()->json(['status' => true]);
+    } else {
+        return response()->json([
+            'status' => false,
+            'errors' => $validator->errors()
+        ]);
+    }
 }
 
 public function logout(){
     Auth::logout();
     return redirect()->route('account.login');
 }
+
+
+public function createJob(){
+    $job_types= JobType::orderBy('name', 'ASC')->where('status',1)->get();
+    $categories= Category::orderBy('name', 'ASC')->where('status',1)->get();
+    return view('frontend.account.job.create',compact('categories','job_types'));
+}
+
+public function saveJob(Request $request){
+    $rules = [
+        'title' => 'required|min:5|max:200',
+        'category' => 'required',
+        'jobType' => 'required',
+        'vacancy' => 'required|integer',
+        'location' => 'required|max:50',
+        'description' => 'required',
+        'company_name' => 'required|min:3|max:40',
+    ];
+
+    $validator = Validator::make($request->all(), $rules);
+
+    if ($validator->passes()) {
+        $job = new Job();
+        $job->title = $request->title;
+        $job->category_id = $request->category;
+        $job->job_type_id = $request->jobType;
+        $job->user_id = Auth::user()->id;
+        $job->vacancy = $request->vacancy;
+        $job->salary = $request->salary;
+        $job->location = $request->location;
+        $job->description = $request->description;
+        $job->benefits = $request->benefits;
+        $job->responsibility = $request->responsibility;
+        $job->qualifications = $request->qualifications;
+        $job->keywords = $request->keywords;
+        $job->experience = $request->experience;
+        $job->company_name = $request->company_name;
+        $job->company_location = $request->company_location;
+        $job->company_website = $request->company_website;
+        $job->save();
+
+        session()->flash('success', 'Job Added Successfully.');
+        return response()->json([
+            'status' => true,
+            'errors' => [],
+        ]);
+    } else {
+        return response()->json([
+            'status' => false,
+            'errors' => $validator->errors()
+        ]);
+    }
+}
+
+public function myJobs(){
+    $jobs= Job::where('user_id',Auth::user()->id)->with('jobType')->paginate(10);
+    return view('frontend.account.job.my-jobs', compact('jobs'));
+}
+
 
 }
 
